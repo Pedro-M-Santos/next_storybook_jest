a465023ad09157fccb3f61d78252e260da4cf290
PEDRO MATIAS DOS SANTOS <pedro.matias@cbm.df.gov.br>
2022-08-11 18:20:48 -0300

    feat: add prettier and editorconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import { Header } from "@components/generics/Header/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-        <Component {...pageProps} />
-
-    </div>
-  )
+	return (
+		<div className="flex flex-col">
+			<Header
+				onCreateAccount={() => console.log("createAccount")}
+				onLogin={() => console.log("login")}
+				onLogout={() => console.log("logout")}
+			/>
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div>Hello World</div>
-  )
-}
+	return <div className="bg-blue-300">Hello World</div>;
+};
 
-export default Home
+export default Home;
