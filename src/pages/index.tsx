f36259a2b0fa1d7e5f42682919409f1ced4f9bea
PEDRO MATIAS DOS SANTOS <pedro.matias@cbm.df.gov.br>
2022-08-12 12:18:08 -0300

    fix: add mui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
+import { Header } from "@components/Header/Header";
 import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
-	return <div>Hello World</div>;
+	return <Header />;
 };
 
 export default Home;
