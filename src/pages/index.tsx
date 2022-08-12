99775efa3447ba832cc358a3efe745689bb9c896
PEDRO MATIAS DOS SANTOS <pedro.matias@cbm.df.gov.br>
2022-08-12 11:49:23 -0300

    reiniciando repositorio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-import { Header } from "@components/Header/Header";
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "src/styles/Home.module.css";
 
 const Home: NextPage = () => {
-	return (
-		<div className="bg-blue-300">
-			<Header />
-		</div>
-	);
+	return <div>Hello World</div>;
 };
 
 export default Home;
