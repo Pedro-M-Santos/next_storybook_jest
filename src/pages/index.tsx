60fa1835452bd5972e01febb512dd79ef97b06a7
MatiasPedro25 <unbmatiaspedro@gmail.com>
2022-08-15 09:19:50 -0300

    add prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div>Hello World</div>;
+};
+
+export default Home;
